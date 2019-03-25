91f84d58bce27f6b431b4e3280388be26fc0b3b1
xdeltoid <rerixo@gmail.com>
2019-03-25 14:58:05 +0000

    changes for left side animation

--- read.js
+++ read.js
@@ -10,10 +10,22 @@ xhttp.send();
 function myFunction(xml) {
 var i;
 var xmlDoc = xml.responseXML;
-var text="";
+var text=["","","",""];
 var x = xmlDoc.getElementsByTagName("scores");
+
 for (i = 0; i <x.length; i++) { 
-  text += x[i].getElementsByTagName("label")[0].childNodes[0].nodeValue;
-}
-document.getElementById("text").innerHTML = text;
+  text[0] += x[i].getElementsByTagName("P1")[0].childNodes[0].nodeValue;
+  text[1] += x[i].getElementsByTagName("P2")[0].childNodes[0].nodeValue;
+  text[2] += x[i].getElementsByTagName("label")[0].childNodes[0].nodeValue;
+  text[3] += x[i].getElementsByTagName("misc")[0].childNodes[0].nodeValue;
+  }
+
+  window.onload = startInterval();
+
+  function startInterval()
+  {
+      setInterval("loadXMLDoc();", 500);
+  }
+
+document.getElementById("text").innerHTML = text[n];
 }
